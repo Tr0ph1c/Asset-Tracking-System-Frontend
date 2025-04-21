4255f17bc163e96ffc645205ac1519e66e8389dc
Tr0ph1c <ahmmahmouda767@gmail.com>
2025-04-21 15:04:45 +0200

    Added home, sign in and up pages to the routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 import Landing from "./pages/Landing";
-//import SignIn from "./pages/SignIn";
-//import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 function App() {
     return (
@@ -11,8 +12,9 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/" element={<Landing />} />
-                    <Route path="/sign-in" element={<Landing />} />
-                    <Route path="/sign-up" element={<Landing />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/home" element={<Home />} />
                 </Routes>
             </Router>
         </>
