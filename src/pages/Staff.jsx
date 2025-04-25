2ffcb2af62566db17fe516f74a45bb88781eac0b
Tr0ph1c <ahmmahmouda767@gmail.com>
2025-04-25 18:51:48 +0300

    changed form data to ref instead of state since we dont want re-renders

--- src/pages/Staff.jsx
+++ src/pages/Staff.jsx
@@ -10,11 +10,10 @@ import { toaster } from "@/components/ui/toaster"
 import { validateEmail, validateRequiredField, staffEndPoint } from "/src/utilities/Helper.js";
 
 const Staff = () => {
-
     // Data
 
     // Main Data In The Form
-    let [formData, setFormData] = useState({
+    let formData = useRef({
         name: '',
         email: '',
         password: ''
@@ -29,14 +28,13 @@ const Staff = () => {
 
     let [isSubmitting, setIsSubmitting] = useState(false);
 
-
     // Logic
     const validate = (name, value) => {
         setErrors(prev => ({
             ...prev,
             [name]: validateRequiredField(name, value)
         }));
-        
+
         if (name === 'email') {
             setErrors(prev => ({
                 ...prev,
@@ -48,24 +46,20 @@ const Staff = () => {
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         validate(name, value);
-        
-        setFormData(prev => ({
-            ...prev,
-            [name]: value
-        }));
-        
+
+        formData.current[name] = value;
     };
 
     const isFormValid = () => {
-        let nameError = validateRequiredField('name', formData.name);
-        let emailError = validateEmail(formData.email);
-        let passwordError = validateRequiredField('password', formData.password);
+        let nameError = validateRequiredField('name', formData.current.name);
+        let emailError = validateEmail(formData.current.email);
+        let passwordError = validateRequiredField('password', formData.current.password);
         setErrors({
             name: nameError,
             email: emailError,
             password: passwordError
         });
-        
+
         // Check for errors
         if (nameError || emailError || passwordError) {
             toaster.create({
@@ -76,10 +70,10 @@ const Staff = () => {
         }
         return true;
     }
-    
+
     const handleSubmit = async (e) => {
         if (!isFormValid()) return;
-        
+
         setIsSubmitting(true);
         try {
             const response = await fetch(staffEndPoint, {
@@ -87,7 +81,7 @@ const Staff = () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(formData)
+                body: JSON.stringify(formData.current)
             });
 
             if (!response.ok) throw new Error('Failed to submit form');
@@ -98,11 +92,12 @@ const Staff = () => {
             });
 
             // Reset form
-            setFormData({
+            formData.current = {
                 name: '',
                 email: '',
                 password: ''
-            });
+            };
+
             setErrors({
                 name: '',
                 email: '',
@@ -122,74 +117,74 @@ const Staff = () => {
 
     // UI
     return (
-      <Flex w="100%" h="100%" justify="center" align="center">
-          <Container maxWidth="30em" background="blue.900" rounded="2xl">
-              <Stack gap="5" py="4em" as="form" onSubmit={handleSubmit}>
-                <Text>Add Staff Member</Text>
-                <Field.Root 
-                    required 
-                    invalid={!!errors.name}
-                >
-                    <Field.Label>Name</Field.Label>
-                    <Input 
-                        name="name"
-                        value={formData.name}
-                        onChange={handleInputChange}
-                        placeholder="Staff Member Name" 
-                        color="black" 
-                        background="whiteAlpha.800" 
-                        variant="solid" />
-                    <Field.ErrorText>{errors.name}</Field.ErrorText>
-                </Field.Root>
-                <Field.Root 
-                    required 
-                    invalid={!!errors.email} 
-                >
-                    <Field.Label>Email</Field.Label>
-                    <Input 
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleInputChange}
-                        placeholder="Staff Member Email" 
-                        color="black" 
-                        background="whiteAlpha.800" 
-                        variant="solid" />
-                    <Field.ErrorText>{errors.email}</Field.ErrorText>
-                </Field.Root>
-                <Field.Root 
-                    required 
-                    invalid={!!errors.password}
-                >
-                    <Field.Label>Password</Field.Label>
-                    <PasswordInput 
-                        name="password"
-                        value={formData.password}
-                        onChange={handleInputChange}
-                        placeholder="Password" 
-                        color="black" 
-                        background="whiteAlpha.800" 
+        <Flex w="100%" h="100%" justify="center" align="center">
+            <Container maxWidth="30em" background="blue.900" rounded="2xl">
+                <Stack gap="5" py="4em" as="form" onSubmit={handleSubmit}>
+                    <Text>Add Staff Member</Text>
+                    <Field.Root
+                        required
+                        invalid={!!errors.name}
+                    >
+                        <Field.Label>Name</Field.Label>
+                        <Input
+                            name="name"
+                            value={formData.current.name}
+                            onChange={handleInputChange}
+                            placeholder="Staff Member Name"
+                            color="black"
+                            background="whiteAlpha.800"
+                            variant="solid" />
+                        <Field.ErrorText>{errors.name}</Field.ErrorText>
+                    </Field.Root>
+                    <Field.Root
+                        required
+                        invalid={!!errors.email}
+                    >
+                        <Field.Label>Email</Field.Label>
+                        <Input
+                            type="email"
+                            name="email"
+                            value={formData.current.email}
+                            onChange={handleInputChange}
+                            placeholder="Staff Member Email"
+                            color="black"
+                            background="whiteAlpha.800"
+                            variant="solid" />
+                        <Field.ErrorText>{errors.email}</Field.ErrorText>
+                    </Field.Root>
+                    <Field.Root
+                        required
+                        invalid={!!errors.password}
+                    >
+                        <Field.Label>Password</Field.Label>
+                        <PasswordInput
+                            name="password"
+                            value={formData.current.password}
+                            onChange={handleInputChange}
+                            placeholder="Password"
+                            color="black"
+                            background="whiteAlpha.800"
+                            variant="solid"
+                        />
+                        <Field.ErrorText>{errors.password}</Field.ErrorText>
+                    </Field.Root>
+
+                    <Button
+                        background="whiteAlpha.800"
                         variant="solid"
-                    />
-                    <Field.ErrorText>{errors.password}</Field.ErrorText>
-                </Field.Root>
-                
-                <Button 
-                    background="whiteAlpha.800" 
-                    variant="solid" 
-                    mt="2em"
-                    type="button"
-                    loading={isSubmitting}
-                    disables={isSubmitting}
-                    onClick={handleSubmit}
-                    loadingText="Adding..."
-                >
-                    Add Member
-                </Button>
-              </Stack>
-          </Container>
-          </Flex>
+                        mt="2em"
+                        type="button"
+                        loading={isSubmitting}
+                        disables={isSubmitting}
+                        onClick={handleSubmit}
+                        loadingText="Adding..."
+                    >
+                        Add Member
+                    </Button>
+                </Stack>
+            </Container>
+        </Flex>
     )
-  }
-  
-  export default Staff
+}
+
+export default Staff
