272fd6e39557df7890029ad393932112e3dfd7b0
Tr0ph1c <ahmmahmouda767@gmail.com>
2025-05-03 10:14:30 +0300

    animations on landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,17 +5,17 @@ const Landing = () => {
     return (
         <>
             <Flex direction="column" gap="1em" justify="center" align="center" h="100%">
-                <Box fontSize="5xl">
+                <Box fontSize="5xl" animation="fade-down 1.5s">
                     Welcome To{" "}
                     <Text color="blue.400" fontWeight="bold" as="span">
                         Assetly
                     </Text>
                 </Box>
-                <Text color="gray.300" as="span" fontSize="2xl">
+                <Text color="gray.300" as="span" fontSize="2xl" animation="fade-down 1s">
                     All your asset managing needs in one place
                 </Text>
                 <Link to="/sign-in">
-                    <Button w="20em" variant="solid" mt="5em">
+                    <Button w="20em" variant="solid" mt="5em" animation="fade-down 0.5s">
                         Get Started
                     </Button>
                 </Link>
